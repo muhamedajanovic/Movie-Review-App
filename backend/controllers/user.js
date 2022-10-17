12a60398be926efd3b60f2e0cecf0132897e6488
Muhamed Ajanovic <m.ajanovic@student.uninp.edu.rs>
2022-10-17 15:50:49 +0200

    Email Verification Token

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -7,5 +7,15 @@ exports.create = async (req, res) => {
     return res.status(401).json({ error: "This email is already in use" });
   const newUser = new User({ name, email, password });
   await newUser.save();
+
+  var transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "06d0900b82f733",
+      pass: "e82d32db4c7118",
+    },
+  });
+
   res.status(201).json({ user: newUser });
 };
---
+++ backend/models/emailVerificationToken.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const emailVerificationTokenSchema = mongoose.Schema({
+  owner: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  token: { type: String, required: true },
+  createAt: { type: Date, expires: 3600, default: Date.now() },
+});
+
+emailVerificationTokenSchema.pre("save", async function (next) {
+  if (this.isModified("token")) {
+    this.token = await bcrypt.hash(this.token, 10);
+  }
+
+  next();
+});
+module.exports = mongoose.model(
+  "emailVerificationToken",
+  emailVerificationTokenSchema
+);
