63438c68cc52439ce496d59967df170a2bd928a4
Muhamed Ajanovic <m.ajanovic@student.uninp.edu.rs>
2022-11-06 15:49:33 +0100

    navbar finished

--- frontend/src/components/user/Navbar.jsx
+++ frontend/src/components/user/Navbar.jsx
@@ -1,14 +1,29 @@
 import React from "react";
+import { BsFillSunFill } from "react-icons/bs";
 
 export default function Navbar() {
   return (
     <div className="bg-secondary">
-      <div className="text-white max-w-screen-xl mx-auto">
-        <div className="flex justify-between items-center"></div>
-        <img src="./logo.png" alt="" className="h-10" />
-        <ul>
-          <li>Login</li>
-        </ul>
+      <div className="max-w-screen-xl mx-auto p-2">
+        <div className="flex justify-between items-center">
+          <img src="./logo.png" alt="" className="h-10" />
+
+          <ul className="flex items-center space-x-4">
+            <li>
+              <button className="bg-dark-subtle p-1 rounded">
+                <BsFillSunFill className="text-secondary" size={24} />
+              </button>
+            </li>
+            <li>
+              <input
+                type="text"
+                className="border-2 border-dark-subtle p-1 rounded bg-transparent text-xl outline-none focus:border-white transition text-white"
+                placeholder="Search here"
+              />
+            </li>
+            <li className="text-white font-semibold text-lg">Login</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
