05629cbbaaede7b0cb36d1da97078cb62ac8c561
Muhamed Ajanovic <m.ajanovic@student.uninp.edu.rs>
2022-11-07 10:59:35 +0100

    forget password form

--- frontend/src/components/auth/ForgetPassword.jsx
+++ frontend/src/components/auth/ForgetPassword.jsx
@@ -1,5 +1,29 @@
 import React from "react";
+import Container from "../Container";
+import CustomLink from "../CustomLink";
+import FormInput from "../form/FormInput";
+import Submit from "../form/Submit";
+import Title from "../form/Title";
 
 export default function ForgetPassword() {
-  return <div>ForgetPassword</div>;
+  return (
+    <div className="fixed inset-0 bg-primary -z-10 flex justify-center items-center">
+      <Container>
+        <form className="bg-secondary rounded p-6 w-96 space-y-6">
+          <Title>Please Enter Your Email</Title>
+          <FormInput
+            label="Email"
+            placeholder="someone@example.com"
+            name="email"
+          ></FormInput>
+          <Submit value="Send Link" />
+
+          <div className="flex justify-between">
+            <CustomLink to="/auth/signin">Sign in</CustomLink>
+            <CustomLink to="/auth/signup">Sign up</CustomLink>
+          </div>
+        </form>
+      </Container>
+    </div>
+  );
 }
