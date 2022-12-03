d64e56c5f6f47605787ebeb2e09bca4d98a3ad2c
Muhamed Ajanovic <m.ajanovic@student.uninp.edu.rs>
2022-12-03 19:58:32 +0100

    toggling theme

--- frontend/src/context/ThemeProvider.js
+++ frontend/src/context/ThemeProvider.js
@@ -2,15 +2,23 @@ import React, { createContext, useEffect } from "react";
 
 export const ThemeContext = createContext();
 
+const defaultTheme = "light";
+const darkTheme = "dark";
+
 export default function ThemeProvider({ children }) {
   const toggleTheme = () => {
-    document.documentElement.classList.add("dark");
-    localStorage.setItem("theme", "dark");
+    const oldTheme = localStorage.getItem("theme");
+    const newTheme = oldTheme === defaultTheme ? darkTheme : defaultTheme;
+
+    document.documentElement.classList.remove(oldTheme);
+    document.documentElement.classList.add(newTheme);
+    localStorage.setItem("theme", newTheme);
   };
 
   useEffect(() => {
     const theme = localStorage.getItem("theme");
-    document.documentElement.classList.add(theme);
+    if (!theme) document.documentElement.classList.add(defaultTheme);
+    else document.documentElement.classList.add(theme);
   });
 
   return (
