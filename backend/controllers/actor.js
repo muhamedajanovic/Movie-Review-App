6a38220a9da380b0dd47e77b792a1263b4e86f0e
Muhamed Ajanovic <m.ajanovic@student.uninp.edu.rs>
2023-01-05 12:56:01 +0100

    Configuring Cloudinary (A Cloud Storage)

--- backend/controllers/actor.js
+++ backend/controllers/actor.js
@@ -1,3 +1,16 @@
+const Actor = require("../models/actor");
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: "ddeixizf1",
+  api_key: "574124421836938",
+  api_secret: "IuSuqKagZcV6K-wp6XiPfh3apFY",
+  secure: true,
+});
+
 exports.createActor = (req, res) => {
-  console.log(req.body);
+  const { name, about, gender } = req.body;
+  const { file } = req;
+
+  const newActor = new Actor({ name, about, gender });
 };
