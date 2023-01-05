fc58ef65f6b09b3ad2ef29777e09cd05a1926232
Muhamed Ajanovic <m.ajanovic@student.uninp.edu.rs>
2023-01-05 12:41:06 +0100

    Middleware to Validate Actor

--- backend/middlewares/validator.js
+++ backend/middlewares/validator.js
@@ -33,6 +33,16 @@ exports.signInValidator = [
     .withMessage("Password must be 8 to 20 characters long"),
 ];
 
+exports.actorInfoValidator = [
+  check("name").trim().not().isEmpty().withMessage("Name is missing"),
+  check("about").trim().not().isEmpty().withMessage("About is required field"),
+  check("gender")
+    .trim()
+    .not()
+    .isEmpty()
+    .withMessage("Gender is required field"),
+];
+
 exports.validate = (req, res, next) => {
   const error = validationResult(req).array();
   if (error.length) return res.json({ error: error[0].msg });
