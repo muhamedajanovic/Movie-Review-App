bd7ae6a220d4ccc3deb03a34f313f137d6761dce
Muhamed Ajanovic <m.ajanovic@student.uninp.edu.rs>
2023-01-06 22:16:36 +0100

    Separate Component to Select Trailer Video

---
+++ frontend/src/components/admin/MovieUpload.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import { FileUploader } from "react-drag-drop-files";
+import { AiOutlineCloudUpload } from "react-icons/ai";
+import { uploadTrailer } from "../../api/movie";
+import { useNotification } from "../../hooks";
+
+export default function MovieUpload() {
+  const [videoSelected, setVideoSelected] = useState(false);
+  const { updateNotification } = useNotification();
+
+  const handleTypeError = (error) => {
+    updateNotification("error", error);
+  };
+
+  const handleChange = async (file) => {
+    const formData = new FormData();
+    formData.append("video", file);
+
+    const res = await uploadTrailer(formData);
+    console.log(res);
+  };
+
+  return (
+    <div className="fixed inset-0 dark:bg-white dark:bg-opacity-50 bg-primary bg-opacity-50 backdrop-blur-sm flex items-center justify-center">
+      <div className="dark:bg-primary bg-white rounded w-[45rem] h-[40rem] overflow-auto">
+        <TrailerSelector
+          visible={!videoSelected}
+          onTypeError={handleTypeError}
+          handleChange={handleChange}
+        />
+      </div>
+    </div>
+  );
+}
+
+const TrailerSelector = ({ visible, handleChange, onTypeError }) => {
+  if (!visible) return null;
+
+  return (
+    <div className="h-full flex items-center justify-center">
+      <FileUploader
+        handleChange={handleChange}
+        onTypeError={onTypeError}
+        types={["mp4", "avi"]}
+      >
+        <div className="w-48 h-48 border border-dashed dark:border-dark-subtle border-light-subtle rounded-full flex flex-col items-center justify-center dark:text-dark-subtle text-secondary cursor-pointer">
+          <AiOutlineCloudUpload size={80} />
+          <p>Drop your file here!</p>
+        </div>
+      </FileUploader>
+    </div>
+  );
+};
