83c94328b4afa3bf1f9d9f109e3796af4ad5f229
Muhamed Ajanovic <m.ajanovic@student.uninp.edu.rs>
2023-01-06 19:10:16 +0100

    Handling Logout from Admin Panel

--- frontend/src/components/admin/Navbar.jsx
+++ frontend/src/components/admin/Navbar.jsx
@@ -4,8 +4,10 @@ import { AiOutlineHome } from "react-icons/ai";
 import { BiMoviePlay } from "react-icons/bi";
 import { FaUserNinja } from "react-icons/fa";
 import { FiLogOut } from "react-icons/fi";
+import { useAuth } from "../../hooks";
 
 export default function Navbar() {
+  const { handleLogout } = useAuth();
   return (
     <nav className="w-48 min-h-screen bg-secondary border-r border-gray-300">
       <div className="flex flex-col justify-between pl-5 h-screen sticky top-0">
@@ -36,7 +38,10 @@ export default function Navbar() {
         </ul>
         <div className="flex flex-col items-start pb-5">
           <span className="font-semibold text-white text-xl">Admin</span>
-          <button className="flex items-center text-dark-subtle text-sm hover:text-white transition space-x-1">
+          <button
+            onClick={handleLogout}
+            className="flex items-center text-dark-subtle text-sm hover:text-white transition space-x-1"
+          >
             <FiLogOut />
             <span>Log out</span>
           </button>
