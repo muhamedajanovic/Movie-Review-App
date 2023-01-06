71f24f9ef80415cc7664725728cc368e818e8139
Muhamed Ajanovic <m.ajanovic@student.uninp.edu.rs>
2023-01-06 16:55:02 +0100

    Fixing Navigation Issue

--- frontend/src/components/auth/Signin.jsx
+++ frontend/src/components/auth/Signin.jsx
@@ -47,9 +47,9 @@ export default function Signin() {
     handleLogin(userInfo.email, userInfo.password);
   };
 
-  useEffect(() => {
-    if (isLoggedIn) navigate("/");
-  }, [isLoggedIn]);
+  // useEffect(() => {
+  //   if (isLoggedIn) navigate("/");
+  // }, [isLoggedIn]);
 
   return (
     <FormContainer>
