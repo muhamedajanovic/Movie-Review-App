f530aaba74412cc18bb2db6b69544d2fc0a64108
Muhamed Ajanovic <m.ajanovic@student.uninp.edu.rs>
2023-01-06 21:49:19 +0100

    Upload Trailer Method

---
+++ frontend/src/api/movie.js
@@ -0,0 +1,19 @@
+import client from "./client";
+
+export const uploadTrailer = async (formData) => {
+  const token = localStorage.getItem("auth-token");
+  try {
+    const { data } = await client.post("/movie/upload-trailer", formData, {
+      headers: {
+        authorization: "Bearer " + token,
+        "content-type": "multipart/form-data",
+      },
+    });
+    return data;
+  } catch (error) {
+    const { response } = error;
+    if (response?.data) return response.data;
+
+    return { error: error.message || error };
+  }
+};
