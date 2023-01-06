9aaefaa2774461e298d9f4cfc2d9504554a86d02
Muhamed Ajanovic <m.ajanovic@student.uninp.edu.rs>
2023-01-06 19:21:18 +0100

    Toggling Options Modal

--- frontend/src/components/admin/Dashboard.jsx
+++ frontend/src/components/admin/Dashboard.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { useState } from "react";
 import { AiOutlinePlus } from "react-icons/ai";
 
 export default function Dashboard() {
+  const [showOptions, setShowOptions] = useState(false);
   return (
     <div className="flex items-center justify-between relative">
       <input
@@ -9,17 +11,21 @@ export default function Dashboard() {
         className="border-2 dark:border-dark-subtle border-light-subtle dark:focus:border-white focus:border-primary dark:text-white transition bg-transparent rounded text-lg p-1 outline-none"
         placeholder="Search Movies..."
       />
-      <button className="flex items-center space-x-2 border-secondary hover:border-primary text-secondary hover:opacity-80 transition font-semibold border-2 rounded text-lg px-3 py-1">
+      <button
+        onClick={() => setShowOptions(!showOptions)}
+        className="flex items-center space-x-2 border-secondary hover:border-primary text-secondary hover:opacity-80 transition font-semibold border-2 rounded text-lg px-3 py-1"
+      >
         <span>Create</span>
         <AiOutlinePlus />
       </button>
 
-      <CreateOptions />
+      <CreateOptions visible={showOptions} />
     </div>
   );
 }
 
-const CreateOptions = () => {
+const CreateOptions = ({ visible }) => {
+  if (!visible) return null;
   return (
     <div className="absolute right-0 top-12 flex flex-col space-y-3 p-5 dark:bg-secondary bg-white drop-shadow-lg rounded">
       <Option>Add Movie</Option>
