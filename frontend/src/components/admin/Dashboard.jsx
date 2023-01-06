b90dfc92f3841a913c0148341cccac9261b0ed6c
Muhamed Ajanovic <m.ajanovic@student.uninp.edu.rs>
2023-01-06 20:26:42 +0100

    Creating Modal Container

--- frontend/src/components/admin/Dashboard.jsx
+++ frontend/src/components/admin/Dashboard.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <div className="fixed inset-0 dark:bg-white dark:bg-opacity-50 bg-primary bg-opacity-50 backdrop-blur-sm flex items-center justify-center">
+      <div className="dark:bg-primary bg-white rounded w-[45rem] h-[40rem] overflow-auto"></div>
+    </div>
+  );
 }
