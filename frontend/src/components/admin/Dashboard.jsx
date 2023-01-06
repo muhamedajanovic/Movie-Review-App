e244120ad3abbccadce11eb6d1a8c05af502d34a
Muhamed Ajanovic <m.ajanovic@student.uninp.edu.rs>
2023-01-06 19:34:45 +0100

    Closing Options With Animation

--- frontend/src/components/admin/Dashboard.jsx
+++ frontend/src/components/admin/Dashboard.jsx
@@ -1,9 +1,9 @@
-import React from "react";
-import { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { AiOutlinePlus } from "react-icons/ai";
 
 export default function Dashboard() {
   const [showOptions, setShowOptions] = useState(false);
+
   return (
     <div className="flex items-center justify-between relative">
       <input
@@ -12,22 +12,56 @@ export default function Dashboard() {
         placeholder="Search Movies..."
       />
       <button
-        onClick={() => setShowOptions(!showOptions)}
+        onClick={() => setShowOptions(true)}
         className="flex items-center space-x-2 border-secondary hover:border-primary text-secondary hover:opacity-80 transition font-semibold border-2 rounded text-lg px-3 py-1"
       >
         <span>Create</span>
         <AiOutlinePlus />
       </button>
 
-      <CreateOptions visible={showOptions} />
+      <CreateOptions
+        visible={showOptions}
+        onClose={() => setShowOptions(false)}
+      />
     </div>
   );
 }
 
-const CreateOptions = ({ visible }) => {
+const CreateOptions = ({ visible, onClose }) => {
+  const container = useRef();
+  const containerID = "options-container";
+
+  useEffect(() => {
+    const handleClose = (e) => {
+      if (!visible) return;
+      const { parentElement, id } = e.target;
+
+      if (parentElement.id === containerID || id === containerID) return;
+
+      if (container.current) {
+        if (!container.current.classList.contains("animate-scale"))
+          container.current.classList.add("animate-scale-reverse");
+      }
+    };
+
+    document.addEventListener("click", handleClose);
+    return () => {
+      document.removeEventListener("click", handleClose);
+    };
+  }, [visible]);
+
   if (!visible) return null;
+
   return (
-    <div className="absolute right-0 top-12 flex flex-col space-y-3 p-5 dark:bg-secondary bg-white drop-shadow-lg rounded animate-scale">
+    <div
+      id={containerID}
+      ref={container}
+      className="absolute right-0 top-12 flex flex-col space-y-3 p-5 dark:bg-secondary bg-white drop-shadow-lg rounded animate-scale"
+      onAnimationEnd={(e) => {
+        if (e.target.classList.contains("animate-scale-reverse")) onClose();
+        e.target.classList.remove("animate-scale");
+      }}
+    >
       <Option>Add Movie</Option>
       <Option>Add Actor</Option>
     </div>
