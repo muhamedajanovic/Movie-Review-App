db07ea801adbac6ef1d2262b3c8bf6f163adfdbc
Muhamed Ajanovic <m.ajanovic@student.uninp.edu.rs>
2023-01-06 16:58:05 +0100

    Creating Navbar

--- frontend/src/components/admin/Dashboard.jsx
+++ frontend/src/components/admin/Dashboard.jsx
@@ -1,5 +1,36 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <nav className="w-48 min-h-screen bg-secondary border-r border-gray-300">
+      <ul className="pl-5">
+        <li>
+          <Link to="/">
+            <img src="./logo.png" alt="logo" className="h-14 p-2" />
+          </Link>
+        </li>
+        <li>
+          <NavItem to="/">Home</NavItem>
+        </li>
+        <li>
+          <NavItem to="/movies">Movies </NavItem>
+        </li>
+        <li>
+          <NavItem to="/actors">Actors</NavItem>
+        </li>
+      </ul>
+    </nav>
+  );
 }
+
+const NavItem = ({ children, to }) => {
+  return (
+    <NavLink
+      className={({ isActive }) => (isActive ? "text-white" : "text-gray-400")}
+      to={to}
+    >
+      {children}
+    </NavLink>
+  );
+};
